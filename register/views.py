9ac09fa7cfb15717c284f24cdd4e2c7f8746c52f
stenli <s.nevrovsky@ya.ru>
2023-10-20 14:21:12 +0300

    add api and register template

--- register/views.py
+++ register/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def hello_world(request):
-    return HttpResponse("Hello, world! You're at the hello index!")
+    return render(request, 'register.html')
