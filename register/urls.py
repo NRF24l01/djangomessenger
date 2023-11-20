d0f84bcad00cf823d25e060f3725f175b48eb520
stenli <s.nevrovsky@ya.ru>
2023-11-20 12:08:42 +0300

    add sigh in

--- register/urls.py
+++ register/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.render_register, name='regren'),
     path('mail_sender/', views.register_sistem, name='check_mail_sender'),
-    path('login/', views.render_login, name='check_mail_sender'),
+    path('login/', views.render_login, name='logren'),
+    path('login/cheker/', views.login_user, name='logus'),
 ]
--- register/views.py
+++ register/views.py
@@ -11,12 +11,16 @@ def register_sistem(request):
     name = request.GET.get("name", None)
     email = request.GET.get("email", None)
     pasw = request.GET.get("pass", None)
-    pasw = request.GET.get("pas1", None)
+    pasw1 = request.GET.get("pass1", None)
+    agree = request.GET.get("agree", False)
+    if not agree:
+        return redirect("/register/?err=Вы не приняли правила сервиса")
+
     if not is_valid_email(email):
         return redirect("/register/?err=Неверная почта")
 
-    if not pasw = (email):
-        return redirect("/register/?err=Неверная почта")
+    if not pasw == pasw1:
+        return redirect("/register/?err=Пароли не совпадают")
 
     if len(Users.objects.filter(email=email)) > 0:
         return redirect("/register/?err=-1")
@@ -36,3 +40,13 @@ def render_register(request):
 def render_login(request):
     err = request.GET.get("err", None)
     return render(request, 'login.html', {"err": err})
+
+def login_user(request):
+    username = request.GET.get("lg", None)
+    pasw = request.GET.get("pass", None)
+
+    if len(Users.objects.filter(email=username, pasw=pasw)) > 0 or len(Users.objects.filter(username=username, pasw=pasw)) > 0:
+        request.session["user"] = username
+        request.session["pass"] = pasw
+    else:
+        return redirect()
